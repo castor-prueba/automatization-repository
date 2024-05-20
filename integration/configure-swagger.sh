e12aa23f643eac7efb5830348c6c950621554de9
JhoanDRojas <163158964+JhoanDRojas@users.noreply.github.com>
2024-05-20 16:00:53 -0500

    Update configure-swagger.sh

--- integration/configure-swagger.sh
+++ integration/configure-swagger.sh
@@ -10,51 +10,105 @@ cd "$SOLUTION_DIR/01.$PROJECT_NAME.Api/$PROJECT_NAME.Function"
 # Agregar paquete Swashbuckle
 dotnet add package Swashbuckle.AspNetCore
 
-# Crear archivo Startup.cs
-cat <<EOL > Startup.cs
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+# Crear archivo Program.cs
+cat <<EOL > Program.cs
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Reflection;
 
-[assembly: FunctionsStartup(typeof($PROJECT_NAME.Function.Startup))]
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "$PROJECT_NAME API", Version = "v1" });
+            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = System.IO.Path.Combine(System.AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+    })
+    .Build();
+
+host.Run();
+EOL
+
+# Crear archivo Function1.cs
+cat <<EOL > Function1.cs
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace $PROJECT_NAME.Function
 {
-    public class Startup : FunctionsStartup
+    public class Function1
     {
-        public override void Configure(IFunctionsHostBuilder builder)
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
         {
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "$PROJECT_NAME API", Version = "v1" });
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = System.IO.Path.Combine(System.AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
 }
 EOL
 
-# Crear archivo FunctionExtensions.cs
-cat <<EOL > FunctionExtensions.cs
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+# Crear archivo SwaggerStartup.cs
+cat <<EOL > SwaggerStartup.cs
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Azure.Functions.Worker.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Linq;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Hosting;
+using System.Threading.Tasks;
 
-namespace $PROJECT_NAME.Function
-{
-    public class FunctionExtensions : IWebJobsStartup
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(workerApp =>
     {
-        public void Configure(IWebJobsBuilder builder)
+        workerApp.UseMiddleware<SwaggerMiddleware>();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddSwaggerGen(c =>
         {
-            builder.AddSwashbuckle(Assembly.GetExecutingAssembly());
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "$PROJECT_NAME API", Version = "v1" });
+            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = System.IO.Path.Combine(System.AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+    })
+    .Build();
+
+host.Run();
+
+public class SwaggerMiddleware : IFunctionsWorkerMiddleware
+{
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        if (context.FunctionDefinition.Name == "swagger.json")
+        {
+            var response = context.GetHttpResponseData();
+            if (response != null)
+            {
+                response.Headers.Add("Content-Type", "application/json");
+                response.Body = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(SwaggerGenerator.GenerateSwagger())));
+                context.SetHttpResponseData(response);
+            }
         }
+
+        await next(context);
     }
 }
 EOL
